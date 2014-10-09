239e9555011501306ccd34b28082133bf1adc2ea
Ya Zhuang <zhuangya@gmail.com>
2014-10-09 15:48:37 +0800

    simple homepage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+$(function () {
+  $('.fittext').fitText();
+
+  var scrollTimer;
+
+  $(window).scroll(function () {
+
+    setTimeout(function () {
+      var windowTop = $(window).scrollTop();
+      var landingHeight = $('#landing').outerHeight();
+      var logoHeight = $('#logo').outerHeight();
+
+      if (windowTop + logoHeight > landingHeight) {
+        $('#logo').hide();
+      } else {
+        $('#logo').show();
+      }
+    }, 100);
+  });
+});
